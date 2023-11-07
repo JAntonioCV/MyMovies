a0210703d5ec497456a1a4c39ec06d60397462a9
Josiel Castillo <josielcastillov@gmail.com>
2023-11-07 16:53:43 -0600

    Capitulo 34

--- app/src/main/java/com/jantonioc/mymovies/DetailActivity.kt
+++ app/src/main/java/com/jantonioc/mymovies/DetailActivity.kt
@@ -2,6 +2,9 @@ package com.jantonioc.mymovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
 import com.bumptech.glide.Glide
 import com.jantonioc.mymovies.databinding.ActivityDetailBinding
 import com.jantonioc.mymovies.model.Movie
@@ -16,10 +19,32 @@ class DetailActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val movie = intent.getParcelableExtra<Movie>(EXTRA_MOVIE)
-        binding.tittle.text = movie?.title
+        title = movie?.title
         Glide.with(this)
             .load("https://image.tmdb.org/t/p/w780/${movie?.backdrop_path}")
             .into(binding.backdrop)
 
+        binding.summary.text = movie?.overview
+        bindDetailInfo(binding.detailinfo,movie)
+
+    }
+
+    private fun bindDetailInfo(detailinfo: TextView, movie: Movie?) {
+        detailinfo.text = buildSpannedString {
+            bold { append("Original language: ")}
+            appendLine(movie?.original_language)
+
+            bold { append("Original Tittle: ")}
+            appendLine(movie?.original_title)
+
+            bold { append("Release Date: ")}
+            appendLine(movie?.release_date)
+
+            bold { append("Popularity: ")}
+            appendLine(movie?.popularity.toString())
+
+            bold { append("Vote Average: ")}
+            appendLine(movie?.vote_average.toString())
+        }
     }
 }
